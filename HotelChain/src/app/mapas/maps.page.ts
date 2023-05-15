e76ead095620445589541464be28761a6c7b2a34
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2023-05-15 18:31:51 +0100

    Modelo novo c/tabs, inicio da tab mapas

---
+++ HotelChain/src/app/mapas/maps.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mapas',
+  templateUrl: 'maps.page.html',
+  styleUrls: ['maps.page.scss']
+})
+export class MapsPage {
+
+  constructor() {}
+
+}
