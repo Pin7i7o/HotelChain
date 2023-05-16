431cbaa6e4a45992f91802d9dbd56a1d434404de
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2023-05-16 21:35:46 +0100

    Tentativa de adicionar ações ao butões e ao input do piso

--- HotelChain/src/app/mapas/maps.page.ts
+++ HotelChain/src/app/mapas/maps.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class MapsPage {
 
+  showImage: boolean = false;
+  floorNumber: number = 0;
+  selectedImage: string = "";
+
   constructor() {}
 
+  toggleImage(){
+    this.showImage = !this.showImage;
+  }
+
+  
+
+  updateSelectedImage() {
+    switch (this.floorNumber) {
+      case 1:
+        this.selectedImage = '/assets/john.png';
+        break;
+      case 2:
+        this.selectedImage = '/assets/logo.png';
+        break;
+      case 0:
+        break;
+      default:
+        this.selectedImage = '/assets/planoPiso.png';
+        break;
+    }
+  }
+
+  ionViewDidEnter() {
+    this.updateSelectedImage();
+  }
+
+  ionViewWillLeave() {
+    this.selectedImage = '/assets/planoPiso.png';
+  }
 }
