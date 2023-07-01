86554c04068170513670da22a08b812973d3309e
Pin7i7o <118998982+Pin7i7o@users.noreply.github.com>
2023-07-01 16:51:26 +0100

    Menu sort prioridade funcional

--- HotelChain/src/app/pages/alertas/alertas.page.ts
+++ HotelChain/src/app/pages/alertas/alertas.page.ts
@@ -9,12 +9,23 @@ import {Alerta, AlertsServiceService} from '../../services/alerts-service.servic
 export class AlertasPage implements OnInit {
 
   alertas: Alerta[] = [];
+  altaAlertas: Alerta[] = [];
+  baixaAlertas: Alerta[] = [];
+  sortedAlertas: Alerta[] = [];
+  selectedPriority: string = '';
 
   constructor(private alertService: AlertsServiceService) { }
 
   async ngOnInit() {
     await this.alertService.init();
     this.alertas = this.alertService.getAlertas();
+    this.alertas.forEach(a => {
+      if (a.prioridade === 'Baixa') {
+        this.baixaAlertas.push(a);
+      } else if (a.prioridade === 'Alta'){
+        this.altaAlertas.push(a);
+      }
+    });
   }
 
   async deleteAlerta(alerta: Alerta) {
@@ -31,4 +42,31 @@ export class AlertasPage implements OnInit {
         return '';
     }
   }
+  
+  sortAlertas(){
+  let sortedArray: Array<Alerta> = [];
+
+  if(this.selectedPriority === 'Baixa'){
+    this.baixaAlertas.forEach(a => {
+      sortedArray.push(a);
+    });
+    this.altaAlertas.forEach(a => {
+      sortedArray.push(a);
+    });
+    this.alertas = sortedArray;
+
+    } else if(this.selectedPriority === 'Alta'){
+      this.altaAlertas.forEach(a => {
+        sortedArray.push(a);
+      });
+      this.baixaAlertas.forEach(a => {
+        sortedArray.push(a);
+      });
+      this.alertas = sortedArray;
+
+    } else{
+      this.alertas = this.alertService.getAlertas();
+    }
+  }
+  
 }
