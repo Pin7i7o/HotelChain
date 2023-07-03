8aa9c936ebd45d57f8072e0480214c9c3fdf6b0f
Pedro1Alves <109022442+Pedro1Alves@users.noreply.github.com>
2023-07-03 18:18:21 +0100

    instrucoes feito

--- HotelChain/src/app/components/header/header.component.ts
+++ HotelChain/src/app/components/header/header.component.ts
@@ -1,16 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
-
+export class HeaderComponent implements OnInit {
   @Input() header!: string;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {}
 
+  redirectToInstructions() {
+    this.router.navigateByUrl('/instrucoes');
+  } 
 }
+
+
