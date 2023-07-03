e004fd8e44be77eef32d9794c179403761e42413
Pedro1Alves <109022442+Pedro1Alves@users.noreply.github.com>
2023-07-03 19:59:58 +0100

    imagens

--- HotelChain/src/app/pages/mapas/maps.page.ts
+++ HotelChain/src/app/pages/mapas/maps.page.ts
@@ -9,23 +9,56 @@ export class MapsPage {
 
   floorNumber: number = 0;
   floor: string = '/assets/icons/interrogação.png';
+  caption: string;
 
-  constructor(){}
+  constructor(){
+    this.floorNumber = 0;
+    this.floor = '/assets/icons/interrogação.png';
+    this.caption = '';
+  }
 
   updateFloor() {
     switch (this.floorNumber) {
       case 1:
-        this.floor = '/assets/logo.png';
+        this.floor = '/assets/mapa/2andar.jpg';
+        this.caption = `9- Sala de arrumações<br>
+        10- Quarto economico<br>
+        11- Quartos comuns<br>
+        12- Sala com maquinas de comida<br>
+        13- Lavandaria comunitaria <br>
+        14- Sala de convivio<br>
+        15- Quarto 5 estrelas<br>
+        16- Ginasio`;
         break;
+      
       case 2:
-        this.floor = '/assets/logoH.png';
+        this.floor = '/assets/mapa/3andar.jpg';
+        this.caption = `17- Sala de arrumações<br>
+        18- Quartos comuns<br>
+        19- Quarto economico<br>
+        20- Mini bar<br>
+        21- Sala infantil<br>
+        22- Sala com maquinas de comida<br>
+        23- Sala de convivio<br>
+        24- Quarto 5 estrelas<br>
+        25- Varanda com jacuzzi`;
         break;
       case 0:
-        this.floor = '/assets/john.png';
-        break;
+        this.floor = '/assets/mapa/1andar.jpg';
+        this.caption = `1- Receção<br>
+        2- Sala de arrumações<br>
+        3- Quarto econômico<br>
+        4- Piscina interior<br>
+        5- Balneários<br>
+        6- Bar<br>
+        7- Refeitório<br>
+        8- Quarto 5 estrelas`;;
+          break;
       default:
-        this.floor = '/assets/icons/interrogação.png';
+        this.floor = '/assets/icons/interrogacao.png';
+        this.caption = 'Legenda desconhecida';
         break;
     }
   }
+  
 }
