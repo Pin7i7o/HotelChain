b73304e12ba4583bb922ab8a5e3a18122ee15145
Pedro1Alves <109022442+Pedro1Alves@users.noreply.github.com>
2023-07-04 17:50:01 +0100

    comentarios

--- HotelChain/src/app/pages/alertas/alertas.page.ts
+++ HotelChain/src/app/pages/alertas/alertas.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Alerta, AlertsServiceService} from '../../services/alerts-service.service';
+import { Alerta, AlertsServiceService } from '../../services/alerts-service.service';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-alertas',
@@ -10,69 +9,70 @@ import { Router } from '@angular/router';
 })
 export class AlertasPage implements OnInit {
 
-  alertas: Alerta[] = [];
-  altaAlertas: Alerta[] = [];
-  baixaAlertas: Alerta[] = [];
-  sortedAlertas: Alerta[] = [];
-  selectedPriority: string = '';
+  alertas: Alerta[] = []; // Declare an array variable named 'alertas' of type 'Alerta' and initialize it as an empty array
+  altaAlertas: Alerta[] = []; // Declare an array variable named 'altaAlertas' of type 'Alerta' and initialize it as an empty array
+  baixaAlertas: Alerta[] = []; // Declare an array variable named 'baixaAlertas' of type 'Alerta' and initialize it as an empty array
+  sortedAlertas: Alerta[] = []; // Declare an array variable named 'sortedAlertas' of type 'Alerta' and initialize it as an empty array
+  selectedPriority: string = ''; // Declare a string variable named 'selectedPriority' and initialize it as an empty string
 
   constructor(private alertService: AlertsServiceService, private router: Router) { }
 
   async ngOnInit() {
-    await this.alertService.init();
-    this.alertas = this.alertService.getAlertas();
+    await this.alertService.init(); // Call the 'init' method of the 'alertService' object asynchronously
+    this.alertas = this.alertService.getAlertas(); // Assign the value of 'getAlertas' method from the 'alertService' object to the 'alertas' variable
     this.alertas.forEach(a => {
       if (a.prioridade === 'Baixa') {
-        this.baixaAlertas.push(a);
-      } else if (a.prioridade === 'Alta'){
-        this.altaAlertas.push(a);
+        this.baixaAlertas.push(a); // Push the alerta into the 'baixaAlertas' array if its priority is 'Baixa'
+      } else if (a.prioridade === 'Alta') {
+        this.altaAlertas.push(a); // Push the alerta into the 'altaAlertas' array if its priority is 'Alta'
       }
     });
   }
 
   async deleteAlerta(alerta: Alerta) {
-    await this.alertService.deleteAlert(alerta.id);
+    await this.alertService.deleteAlert(alerta.id); // Call the 'deleteAlert' method of the 'alertService' object asynchronously with the 'id' of the alerta to be deleted
   }
 
-  getIcon(alerta: Alerta): string{
+  getIcon(alerta: Alerta): string {
     switch (alerta.tipo) {
       case 'Sensor':
-        return 'help-circle-outline';
+        return 'help-circle-outline'; // Return the icon name 'help-circle-outline' if the alerta type is 'Sensor'
       case 'Manutencao':
-        return 'hammer';
+        return 'hammer'; // Return the icon name 'hammer' if the alerta type is 'Manutencao'
       default:
-        return '';
+        return ''; // Return an empty string if the alerta type is neither 'Sensor' nor 'Manutencao'
     }
   }
-  
-  sortAlertas(){
-  let sortedArray: Array<Alerta> = [];
 
-  if(this.selectedPriority === 'Baixa'){
-    this.baixaAlertas.forEach(a => {
-      sortedArray.push(a);
-    });
-    this.altaAlertas.forEach(a => {
-      sortedArray.push(a);
-    });
-    this.alertas = sortedArray;
+  sortAlertas() {
+    let sortedArray: Array<Alerta> = []; // Declare a new array variable named 'sortedArray' of type 'Alerta' and initialize it as an empty array
 
-    } else if(this.selectedPriority === 'Alta'){
+    if (this.selectedPriority === 'Baixa') {
+      this.baixaAlertas.forEach(a => {
+        sortedArray.push(a); // Push each alerta from 'baixaAlertas' array into the 'sortedArray'
+      });
       this.altaAlertas.forEach(a => {
-        sortedArray.push(a);
+        sortedArray.push(a); // Push each alerta from 'altaAlertas' array into the 'sortedArray'
+      });
+      this.alertas = sortedArray; // Assign the sorted array to the 'alertas' array
+
+    } else if (this.selectedPriority === 'Alta') {
+      this.altaAlertas.forEach(a => {
+        sortedArray.push(a); // Push each alerta from 'altaAlertas' array into the 'sortedArray'
       });
       this.baixaAlertas.forEach(a => {
-        sortedArray.push(a);
+        sortedArray.push(a); // Push each alerta from 'baixaAlertas' array into the 'sortedArray'
       });
-      this.alertas = sortedArray;
+      this.alertas = sortedArray; // Assign the sorted array to the 'alertas' array
 
-    } else{
-      this.alertas = this.alertService.getAlertas();
+    } else {
+      this.alertas = this.alertService.getAlertas(); // If no priority is selected, assign the unsorted 'alertas' array from 'alertService' to the 'alertas' variable
     }
   }
-  
+
   goToAlertaDetailsPage(alerta: Alerta) {
-    this.router.navigate(['/problema'], { queryParams: alerta });
+    this.router.navigate(['/problema'], { queryParams: alerta }); // Navigate to the '/problema' route with the alerta as a query parameter
   }
-  
+
 }
+
--- HotelChain/src/app/services/alerts-service.service.ts
+++ HotelChain/src/app/services/alerts-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 
 export interface Alerta {
-  id : number;
+  id: number;
   user: string;
   data: Date;
   descricao: string;
@@ -21,99 +21,101 @@ export class AlertsServiceService {
 
   private alertas: Alerta[];
 
-  constructor(private storage: Storage) { 
+  constructor(private storage: Storage) {
     this.alertas = [];
-    this.init();
+    this.init(); // Call the initialization method when the service is created
   }
 
   async init() {
-    const storage = await this.storage.create();
-    const alertas = await storage.get('alertas');
+    const storage = await this.storage.create(); // Create the storage instance
+    const alertas = await storage.get('alertas'); // Retrieve alertas from storage
     if (alertas) {
-      this.alertas = alertas;
+      this.alertas = alertas; // If alertas exist in storage, assign them to the service's alertas array
     } else {
-      this.defaultAlerts();
+      this.defaultAlerts(); // If alertas don't exist in storage, set default alertas
     }
   }
 
   getAlertas(): Alerta[] {
-    return this.alertas
+    return this.alertas; // Return the alertas array
   }
 
   async defaultAlerts() {
     const defaultAlerts: Alerta[] = [
-      { 
-        id : 1,
-        user: 'guest', 
-        data: new Date(), 
-        descricao: 'Novo Sensor Adicionado', 
-        prioridade: 'Alta', 
-        tipo: 'Sensor', 
+      // Define default alertas
+      {
+        id: 1,
+        user: 'guest',
+        data: new Date(),
+        descricao: 'Novo Sensor Adicionado',
+        prioridade: 'Alta',
+        tipo: 'Sensor',
         infraestrutura: 258,
         equipamento: 1298,
         controlo: 'Temp',
-        concluido: false 
+        concluido: false
       },
-      { 
-        id : 2,
-        user: 'guest', 
-        data: new Date(), 
-        descricao: 'Frigorífico Desligado', 
-        prioridade: 'Alta', 
-        tipo: 'Manutencao', 
+      {
+        id: 2,
+        user: 'guest',
+        data: new Date(),
+        descricao: 'Frigorífico Desligado',
+        prioridade: 'Alta',
+        tipo: 'Manutencao',
         infraestrutura: 322,
         equipamento: 945,
         controlo: 'Vol',
-        concluido: false 
+        concluido: false
       },
-      { 
-        id : 3,
-        user: 'guest', 
-        data: new Date(), 
-        descricao: 'Televisão Ligada', 
-        prioridade: 'Baixa', 
-        tipo: 'Manutencao', 
+      {
+        id: 3,
+        user: 'guest',
+        data: new Date(),
+        descricao: 'Televisão Ligada',
+        prioridade: 'Baixa',
+        tipo: 'Manutencao',
         infraestrutura: 101,
         equipamento: 2150,
         controlo: 'Temp',
-        concluido: false 
+        concluido: false
       },
-      { 
-        id : 4,
-        user: 'guest', 
-        data: new Date(), 
-        descricao: 'Novo Sensor Adicionado', 
-        prioridade: 'Alta', 
-        tipo: 'Sensor', 
+      {
+        id: 4,
+        user: 'guest',
+        data: new Date(),
+        descricao: 'Novo Sensor Adicionado',
+        prioridade: 'Alta',
+        tipo: 'Sensor',
         infraestrutura: 120,
         equipamento: 654,
         controlo: 'Vol',
-        concluido: false 
+        concluido: false
       },
-      { 
-        id : 5,
-        user: 'guest', 
-        data: new Date(), 
-        descricao: 'Chuveiro com uma fuga', 
-        prioridade: 'Alta', 
-        tipo: 'Manutencao', 
+      {
+        id: 5,
+        user: 'guest',
+        data: new Date(),
+        descricao: 'Chuveiro com uma fuga',
+        prioridade: 'Alta',
+        tipo: 'Manutencao',
         infraestrutura: 488,
         equipamento: 2469,
         controlo: 'None',
-        concluido: false 
+        concluido: false
       }
-    ]
+    ];
 
-    this.alertas = defaultAlerts;
+    this.alertas = defaultAlerts; // Assign default alertas to the service's alertas array
 
-    await this.storage?.set('alertas', defaultAlerts);
+    await this.storage?.set('alertas', defaultAlerts); // Store default alertas in storage
   }
 
   async deleteAlert(id: number) {
-    const index = this.alertas.findIndex(a => a.id === id);
+    const index = this.alertas.findIndex(a => a.id === id); // Find the index of the alerta with the given id
     if (index >= 0) {
-        this.alertas.splice(index, 1);
-        await this.storage.set('alertas', this.alertas);
+      this.alertas.splice(index, 1); // Remove the alerta from the alertas array
+      await this.storage.set('alertas', this.alertas); // Update the alertas stored in storage
     }
+  }
 }
-}
+
